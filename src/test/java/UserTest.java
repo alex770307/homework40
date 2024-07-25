754f9a1c0e322f562cd19139c3d9da1d159215fa
alex_ <alex_7377@hotmail.com>
2024-07-25 07:35:19 +0200

    Initial commit

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getLogin() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setLogin() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void isValidPassword() {
+    }
+}
